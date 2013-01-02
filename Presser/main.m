50adba6b9bc36d9577293b12e6d5b0282485c68e
Tim Johnsen <timonus@gmail.com>
2013-01-01 17:42:45 -0700

    Initial Commit

---
+++ Presser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Presser
+//
+//  Created by Tim Johnsen on 1/1/13.
+//  Copyright (c) 2013 tijo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
